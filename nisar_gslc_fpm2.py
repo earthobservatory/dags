9c7b8da5687506c02b543b596615c07e91a63c0b
shitong <shitong@eos-18181ChinShiTong.local>
2026-03-25 12:14:04 +0800

    letz go

---
+++ nisar_gslc_fpm2.py
@@ -0,0 +1,257 @@
+from airflow.decorators import dag
+from datetime import datetime
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow import DAG
+from airflow.models import Variable
+import json
+from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.operators.python import PythonOperator
+from airflow.contrib.sensors.python_sensor import PythonSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.models import DagRun
+from airflow.utils.trigger_rule import TriggerRule
+from copy import deepcopy
+from airflow.operators.dummy import DummyOperator
+
+
+import requests
+name = "NI_GSLC_FPM2"
+
+
+def failure_callback(context):
+    """
+    Combined failure callback that:
+    1. Sends a Slack alert when a task fails.
+    2. Updates the job status to 'failed' via an HTTP request.
+    """
+    dag_run: DagRun = context['dag_run']
+    dag_run_id = dag_run.run_id if dag_run else "unknown"
+
+    # 1. Send Slack Notification
+    try:
+        slack_msg = f"""
+            :red_circle: Task Failed. Please go to Airflow for more details.
+            *Task*: {context.get('task_instance').task_id}
+            *Dag*: {context.get('task_instance').dag_id}
+            *Execution Time*: {context.get('execution_date')}
+            *Log Url*: {context.get('task_instance').log_url}
+        """
+        slack_alert = SlackWebhookOperator(
+            task_id='slack_failed_alert',
+            slack_webhook_conn_id='slack_webhook_dpm2',
+            message=slack_msg,
+            username='airflow',
+            channel='#dpm2-sarfinder-aws-hpc'
+        )
+        slack_alert.execute(context=context)
+        print("Slack alert sent successfully.")
+    except Exception as slack_error:
+        print(f"Failed to send Slack alert: {slack_error}")
+
+    # 2. Update Job Status to 'failed'
+    try:
+        fail_job_status = SimpleHttpOperator(
+            task_id='update_job_status',
+            http_conn_id='sarfinder',  # Define this connection in Airflow
+            endpoint='api/sarfinder/airflow/task/update/',  # Replace with your actual endpoint
+            method='POST',
+            headers={"Content-Type": "application/json"},
+            data=json.dumps({
+                # "request_id": "{{ var.json[run_id].request_id }}",  # Access run_id from XCom
+                "status": "failed",
+                "dag_run_id": "{{ run_id }}"
+            })
+        )
+
+        fail_job_status.execute(context=context)
+
+        print(f"Updated status to failed for DAG run {dag_run_id}")
+    except Exception as http_error:
+        print(f"Failed to update job status: {http_error}")
+
+
+# def failure_callback(context):
+#     """
+#     Callback function to update job status to 'failed' when a task fails.
+#     """
+#     dag_run: DagRun = context['dag_run']
+#     dag_run_id = dag_run.run_id if dag_run else "unknown"
+
+#     payload = json.dumps({"status": "failed", "dag_run_id": dag_run_id})
+
+#     headers = {"Content-Type": "application/json"}
+
+#     try:
+#         response = requests.post("http://sarfinder/api/sarfinder/airflow/task/update/",
+#                                  data=payload, headers=headers)
+#         response.raise_for_status()
+#         print(f"Updated status to failed for DAG run {dag_run_id}: {response.text}")
+#     except requests.RequestException as e:
+#         print(f"Failed to update job status: {e}")
+
+def cleanup_variables(**kwargs):
+    run_id = kwargs['run_id']
+    Variable.delete(run_id)
+
+
+def set_and_store_variables(**kwargs):
+    run_id = kwargs['run_id']
+    variables = Variable.get(f"{name}_variables")
+    Variable.set(run_id, variables)
+    prevent_selection_files = Variable.get(run_id, deserialize_json=True).get('selection_download', None)
+    return prevent_selection_files
+
+with (DAG(
+        dag_id=name,
+        schedule_interval=None,
+        start_date=datetime(2022, 1, 1),
+        catchup=False,
+        on_failure_callback=failure_callback
+) as dag):
+
+    set_variable_task = PythonOperator(
+        task_id='set_and_store_variables',
+        python_callable=set_and_store_variables,
+        provide_context=True
+    )
+
+    prepare_directory = SSHOperator(
+        task_id="00a_prepare_directory_fpm2.sh",
+        ssh_conn_id='ssh',
+#        command=f'source ~/.bash_profile; echo VARIABLES: {json.dumps({{ var.value[run_id] }})}; 00a_prepare_directory_dpm2.sh {json.dumps({{ var.value[run_id] }})}',
+        command="source ~/.bash_profile; export VARIABLE=$(echo '{{ var.value[run_id] }}' | tr -d '\n')  && 00a_prepare_directory_fpm2.sh \"$VARIABLE\"",
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+#    get_dem = SSHOperator(
+#        task_id="00_get_dem_adv.sh",
+#        ssh_conn_id='ssh',
+#        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 00_get_dem_adv.sh ""',
+#        cmd_timeout=None,
+#        conn_timeout=None
+#    )
+
+    update_download_config = SSHOperator(
+        task_id="01a_update_download_config.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 01a_update_download_config.sh ""',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    download = SSHOperator(
+        task_id="01b_download.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 01b_download.sh ""',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    symlink = SSHOperator(
+        task_id="02a_symlink_data.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 02a_symlink_data.sh ""',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    stackproc_runfile_setup = SSHOperator(
+        task_id="03_create_run_script_nisar.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 03_create_run_script_nisar.sh ""',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+
+    auto_control_run1 = SSHOperator(
+        task_id="04_auto_control.sh_start_run1.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 04_auto_control.sh "{{ var.json[run_id].dir_name }}_run1" "start" "run1" "run1"',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    auto_control_run2 = SSHOperator(
+        task_id="04_auto_control.sh_start_run2.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 04_auto_control.sh "{{ var.json[run_id].dir_name }}_run2" "start" "run2" "run2"',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    auto_control_run3 = SSHOperator(
+        task_id="04_auto_control.sh_start_run3.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 04_auto_control.sh "{{ var.json[run_id].dir_name }}_run3" "start" "run3" "run3"',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+    auto_control_run4 = SSHOperator(
+        task_id="04_auto_control.sh_start_run4.sh",
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; 04_auto_control.sh "{{ var.json[run_id].dir_name }}_run4" "start" "run4" "run4"',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+
+    send_slack = SlackWebhookOperator(
+        task_id='send_slack_notifications',
+        slack_webhook_conn_id='slack_webhook_dpm2',
+        message=':blob_excited:On your MacBook, run the following scripts to download DPM2 products:blob_excited:\n```\nscp -r aws-hpc2:/home/ubuntu/urgent_response/{{ var.json[run_id].dir_name }}/dpm2/probGV/\*tif .\n```\n \n',
+        channel='#dpm2-sarfinder-aws-hpc',
+        username='airflow'
+    )
+
+    update_job_status = SimpleHttpOperator(
+        task_id='update_job_status',
+        http_conn_id='sarfinder',  # Define this connection in Airflow
+        endpoint='api/sarfinder/airflow/task/update/',  # Replace with your actual endpoint
+        method='POST',
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({
+            # "request_id": "{{ var.json[run_id].request_id }}",  # Access run_id from XCom
+            "status": "success",
+            "dag_run_id": "{{ run_id }}"
+        })
+    )
+
+    archive_task = SSHOperator(
+        task_id='response_archive',
+        ssh_conn_id='ssh',
+        command='source ~/.bash_profile; cd urgent_response/{{ var.json[run_id].dir_name }}; archive_responses.sh -f {{ var.json[run_id].dir_name }}',
+        cmd_timeout=None,
+        conn_timeout=None
+    )
+
+
+    cleanup_task = PythonOperator(
+        task_id='cleanup_variables',
+        python_callable=cleanup_variables,
+        provide_context=True,
+        trigger_rule=TriggerRule.ALL_SUCCESS  # Ensures task runs only if all upstream tasks succeed
+
+    )
+
+    set_variable_task >> prepare_directory >> update_download_config >> \
+    download >> symlink >> \
+    stackproc_runfile_setup >> \
+    auto_control_run1 >> auto_control_run2 >> auto_control_run3 >> auto_control_run4 >> \
+    send_slack >> update_job_status >> archive_task >> cleanup_task
+
+
+    # Break here to wait, use a Deferring Task (Sensor etc) to check for variable change
+    # Do:
+    # update_selection_file
+    # download
+    # symlink
+    # stackproc_runfile_setup_update
+    # auto_control_runu1 to auto_control_runu6
+    # generate_slcstk2cor (no need update)
+    # auto_control_run_dpm2_4 >> auto_control_run_dpm2_5 >> auto_control_run_dpm2_6 >> auto_control_run_dpm2_7 >> auto_control_run_dpm2_8 >> \
+    # send_slack >> upload_greyscale >> update_job_status >> cleanup_task
